d0400cd14ddbb2834532c7d876e617f7e7163ee0
Werner Laurensse <werner@alman.ax>
2019-12-10 01:29:15 +0100

    Update test applications.

--- cmd/foxtrot/main.go
+++ cmd/foxtrot/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/wrnrlr/foxtrot/app"
+	"os"
 )
 
 func main() {
-	app.RunUI()
+	path := ""
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	app.RunUI(path)
 }
