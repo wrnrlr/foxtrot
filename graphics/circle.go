c03cc8cb59b8301ecc5cb94db7b77f066c251332
Werner Laurensse <werner@alman.ax>
2019-12-10 01:25:06 +0100

    Revert "Move defenitions of cell events to event.go file."
    This reverts commit ccc58c7d

--- graphics/circle.go
+++ graphics/circle.go
@@ -1,71 +1,12 @@
 package graphics
 
-import (
-	"gioui.org/f32"
-	"gioui.org/op"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
-	"github.com/corywalker/expreduce/expreduce/atoms"
-	"github.com/wrnrlr/foxtrot/util"
-)
-
-func toCircle(e *atoms.Expression) (*Circle, error) {
-	c := &Circle{}
-	return c, nil
-}
+import "gioui.org/f32"
 
 type Circle struct {
 	center f32.Point
 	radius float32
 }
 
-func (c Circle) Draw(ctx *context, ops *op.Ops) {
-	w, h := float32(100), float32(100)
-	const curve = 0.55228475 // 4*(sqrt(2)-1)/3
-	rr := float32(100) * .5
-	rrect(ops, w, h, rr, rr, rr, rr)
-	paint.ColorOp{util.Black}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: float32(100), Y: 100}}}.Add(ops)
-}
-
-func (c Circle) BoundingBox() (bbox f32.Rectangle) {
-	return bbox
-}
-
 type Ellipse struct{}
 
 type Arc struct{}
-
-func rrect(ops *op.Ops, width, height, se, sw, nw, ne float32) {
-	w, h := float32(width), float32(height)
-	const c = 0.55228475 // 4*(sqrt(2)-1)/3
-	var b clip.Path
-	b.Begin(ops)
-	b.Move(f32.Point{X: w, Y: h - se})
-	b.Cube(f32.Point{X: 0, Y: se * c}, f32.Point{X: -se + se*c, Y: se}, f32.Point{X: -se, Y: se}) // SE
-	b.Line(f32.Point{X: sw - w + se, Y: 0})
-	b.Cube(f32.Point{X: -sw * c, Y: 0}, f32.Point{X: -sw, Y: -sw + sw*c}, f32.Point{X: -sw, Y: -sw}) // SW
-	b.Line(f32.Point{X: 0, Y: nw - h + sw})
-	b.Cube(f32.Point{X: 0, Y: -nw * c}, f32.Point{X: nw - nw*c, Y: -nw}, f32.Point{X: nw, Y: -nw}) // NW
-	b.Line(f32.Point{X: w - ne - nw, Y: 0})
-	b.Cube(f32.Point{X: ne * c, Y: 0}, f32.Point{X: ne, Y: ne - ne*c}, f32.Point{X: ne, Y: ne}) // NE
-	// Return to origin
-	b.Move(f32.Point{X: -w, Y: -ne})
-	const scale = 0.85
-	b.Move(f32.Point{X: w * (1 - scale) * .5, Y: h * (1 - scale) * .5})
-	w *= scale
-	h *= scale
-	se *= scale
-	sw *= scale
-	nw *= scale
-	ne *= scale
-	b.Move(f32.Point{X: w, Y: h - se})
-	b.Cube(f32.Point{X: 0, Y: se * c}, f32.Point{X: -se + se*c, Y: se}, f32.Point{X: -se, Y: se}) // SE
-	b.Line(f32.Point{X: sw - w + se, Y: 0})
-	b.Cube(f32.Point{X: -sw * c, Y: 0}, f32.Point{X: -sw, Y: -sw + sw*c}, f32.Point{X: -sw, Y: -sw}) // SW
-	b.Line(f32.Point{X: 0, Y: nw - h + sw})
-	b.Cube(f32.Point{X: 0, Y: -nw * c}, f32.Point{X: nw - nw*c, Y: -nw}, f32.Point{X: nw, Y: -nw}) // NW
-	b.Line(f32.Point{X: w - ne - nw, Y: 0})
-	b.Cube(f32.Point{X: ne * c, Y: 0}, f32.Point{X: ne, Y: ne - ne*c}, f32.Point{X: ne, Y: ne}) // NE
-	b.End().Add(ops)
-}
