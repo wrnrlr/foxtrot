f120aad25f20cec3861866e62e45edd84b561d6b
Werner Laurensse <werner@alman.ax>
2019-12-10 01:27:24 +0100

    Add x,y methods to context.

--- graphics/context.go
+++ graphics/context.go
@@ -1,11 +1,23 @@
 package graphics
 
 import (
-	"gioui.org/layout"
-	"gioui.org/text"
+	"gioui.org/f32"
 )
 
-type Context struct {
-	*layout.Context
-	Shaper *text.Shaper
+//type Context struct {
+//	*layout.Context
+//	Shaper *text.Shaper
+//}
+
+type context struct {
+	BBox  f32.Rectangle
+	style *Style
+}
+
+func (c context) x() float32 {
+	return 0
+}
+
+func (c context) y() float32 {
+	return 0
 }
