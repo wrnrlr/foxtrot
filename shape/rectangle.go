55b603085c39c694e5bbdbe5ef9953b07a944809
Werner Laurensse <werner@alman.ax>
2020-01-11 19:48:14 +0100

    Use lineWidth argument instead of fixed scalar value to determine border size

--- shape/rectangle.go
+++ shape/rectangle.go
@@ -7,7 +7,6 @@ import (
 )
 
 func StrokeRectangle(p1, p2 f32.Point, lineWidth float32, ops *op.Ops) {
-	scale := float32(0.80) // base value on lineWidth
 	var path clip.Path
 	path.Begin(ops)
 	path.Move(p1)
@@ -15,9 +14,9 @@ func StrokeRectangle(p1, p2 f32.Point, lineWidth float32, ops *op.Ops) {
 	path.Line(f32.Point{X: 0, Y: p2.Y})
 	path.Line(f32.Point{X: -p2.X, Y: 0})
 	path.Line(f32.Point{X: 0, Y: -p2.Y})
-	path.Move(f32.Point{X: p2.X * (1 - scale) * 0.5, Y: p2.Y * (1 - scale) * 0.5})
-	p2.X *= scale
-	p2.Y *= scale
+	path.Move(f32.Point{X: lineWidth, Y: lineWidth})
+	p2.X -= lineWidth * 2
+	p2.Y -= lineWidth * 2
 	path.Line(f32.Point{X: 0, Y: p2.Y})
 	path.Line(f32.Point{X: p2.X, Y: 0})
 	path.Line(f32.Point{X: 0, Y: -p2.Y})
