7adfa5e7ded8495bc0bf2ae70d182b3e69af0346
Werner Laurensse <werner@alman.ax>
2020-01-19 06:40:02 +0100

    Add Min and Max to util

--- util/f32.go
+++ util/f32.go
@@ -5,3 +5,11 @@ import "math"
 func Absf32(x float32) float32 {
 	return math.Float32frombits(math.Float32bits(x) &^ (1 << 31))
 }
+
+func Min(n, m float32) float32 {
+	return float32(math.Min(float64(n), float64(m)))
+}
+
+func Max(n, m float32) float32 {
+	return float32(math.Min(float64(n), float64(m)))
+}
