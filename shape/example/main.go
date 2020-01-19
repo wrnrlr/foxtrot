f9e2bcde7a97604c85d3e9d1cb55bf3ce834d593
Werner Laurensse <werner@alman.ax>
2020-01-19 06:40:31 +0100

    Update shape example

--- shape/example/main.go
+++ shape/example/main.go
@@ -8,6 +8,7 @@ import (
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
+	"gioui.org/unit"
 	"github.com/wrnrlr/foxtrot/shape"
 	"image/color"
 )
@@ -31,7 +32,7 @@ func main() {
 				return
 			case system.FrameEvent:
 				gtx.Reset(e.Config, e.Size)
-				painting(gtx.Ops)
+				painting(gtx)
 				e.Frame(gtx.Ops)
 			}
 		}
@@ -39,55 +40,59 @@ func main() {
 	app.Main()
 }
 
-func painting(ops *op.Ops) {
+func painting(gtx *layout.Context) {
+	defaultWidth := float32(gtx.Px(unit.Sp(2)))
+
 	var stack op.StackOp
-
-	stack.Push(ops)
-	line1 := []f32.Point{{X: 10, Y: 10}, {X: 110, Y: 10}, {X: 210, Y: 10}}
-	shape.StrokeLine(line1, ops)
-	paint.ColorOp{red}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	stack.Push(gtx.Ops)
+	line := shape.Line{{X: 10, Y: 10}, {X: 110, Y: 10}, {X: 210, Y: 10}}
+	bbox := line.Stroke(unit.Sp(5), gtx)
+	paint.ColorOp{red}.Add(gtx.Ops)
+	paint.PaintOp{bbox}.Add(gtx.Ops)
 	stack.Pop()
 
-	stack.Push(ops)
-	shape.StrokeCircle(f32.Point{70, 70}, 40, 4, ops)
-	paint.ColorOp{blue}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	stack.Push(gtx.Ops)
+	circle := shape.Circle{f32.Point{70, 70}, 40}
+	bbox = circle.Stroke(defaultWidth, gtx)
+	paint.ColorOp{blue}.Add(gtx.Ops)
+	paint.PaintOp{bbox}.Add(gtx.Ops)
 	stack.Pop()
 
-	stack.Push(ops)
-	shape.FillCircle(f32.Point{160, 70}, 40, ops)
-	paint.ColorOp{blue}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	stack.Push(gtx.Ops)
+	circle = shape.Circle{f32.Point{160, 70}, 40}
+	bbox = circle.Fill(gtx)
+	paint.ColorOp{blue}.Add(gtx.Ops)
+	paint.PaintOp{bbox}.Add(gtx.Ops)
 	stack.Pop()
 
-	//stack.Push(ops)
-	//shape.StrokeRectangle(f32.Point{40, 160}, f32.Point{100, 60}, 10, ops)
-	//paint.ColorOp{green}.Add(ops)
-	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	//stack.Push(gtx)
+	//shape.StrokeRectangle(f32.Point{40, 160}, f32.Point{100, 60}, 10, gtx)
+	//paint.ColorOp{green}.Add(gtx)
+	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(gtx)
 	//stack.Pop()
 
-	stack.Push(ops)
-	shape.StrokeRectangle(f32.Point{200, 160}, f32.Point{300, 400}, 10, ops)
-	paint.ColorOp{green}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	stack.Push(gtx.Ops)
+	rect := shape.Rectangle{f32.Point{200, 160}, f32.Point{300, 400}}
+	bbox = rect.Stroke(10, gtx)
+	paint.ColorOp{green}.Add(gtx.Ops)
+	paint.PaintOp{bbox}.Add(gtx.Ops)
 	stack.Pop()
 
-	//stack.Push(ops)
-	//shape.StrokeRectangle(f32.Point{40, 360}, f32.Point{100, 100}, 10, ops)
-	//paint.ColorOp{green}.Add(ops)
-	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	//stack.Push(gtx)
+	//shape.StrokeRectangle(f32.Point{40, 360}, f32.Point{100, 100}, 10, gtx)
+	//paint.ColorOp{green}.Add(gtx)
+	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(gtx)
 	//stack.Pop()
 	//
-	//stack.Push(ops)
-	//shape.FillRectangle(f32.Point{40, 240}, f32.Point{30, 60}, ops)
-	//paint.ColorOp{green}.Add(ops)
-	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	//stack.Push(gtx)
+	//shape.FillRectangle(f32.Point{40, 240}, f32.Point{30, 60}, gtx)
+	//paint.ColorOp{green}.Add(gtx)
+	//paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(gtx)
 	//stack.Pop()
 
-	stack.Push(ops)
-	shape.FillTriangle(f32.Point{300, 10}, f32.Point{360, 10}, f32.Point{340, 60}, ops)
-	paint.ColorOp{red}.Add(ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: 600, Y: 600}}}.Add(ops)
+	stack.Push(gtx.Ops)
+	shape.FillTriangle(f32.Point{300, 10}, f32.Point{360, 10}, f32.Point{340, 60}, gtx)
+	paint.ColorOp{red}.Add(gtx.Ops)
+	paint.PaintOp{bbox}.Add(gtx.Ops)
 	stack.Pop()
 }
